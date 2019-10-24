636efc2828be29a522f4618e53cee42f854e05c2
github username <oscar8atech@gmail.com>
2019-10-24 14:35:08 -0400

    login failure touches

--- app/jobs/close_poll_job.rb
+++ app/jobs/close_poll_job.rb
@@ -4,10 +4,8 @@ class ClosePollJob < ApplicationJob
   def perform(*args)
     # Do something later
     poll = args.first
-    # duration = args.second
-
+    
     #poll[:is_active] = false
-
     poll.update(is_active: false)
   end
 end
