6e390504f396815b0b17dbbf47bf2f41e41d2114
github username <oscar8atech@gmail.com>
2019-10-24 14:06:48 -0400

    last push for backend

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,10 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :vote_option
-  has_many :polls, through: :users
 
+  # validates_uniqueness_of :poll_id, scope: [:user_id, :vote_option_id]
   # validates_uniqueness_of :vote_option, scope: [:vote_option_id, :user_id]
-  validates_uniqueness_of :user, scope: [:poll_id, :user_id]
+  validates_uniqueness_of :user_id, scope: [:vote_option_id, :user_id]
 
   
 end
