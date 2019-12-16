41710da324d74d621f7cec8fee7194b780949c26
github username <oscar8atech@gmail.com>
2019-12-16 15:25:51 -0500

    tech interview changes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,5 +6,13 @@ class Vote < ApplicationRecord
   # validates_uniqueness_of :vote_option, scope: [:vote_option_id, :user_id]
   validates_uniqueness_of :user_id, scope: [:vote_option_id, :user_id]
 
-  
+  validates :unique_poll_vote?, inclusion: [true] 
+
+  def poll
+    vote_option.poll
+  end
+
+  def unique_poll_vote?
+    poll.votes.where(user_id: user.id).empty?
+  end
 end
