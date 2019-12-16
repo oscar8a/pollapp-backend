81dc1d9f1e01575794ad40727ca47d57a3a4df5e
github username <oscar8atech@gmail.com>
2019-12-16 02:38:32 -0500

    Create unique vote method w/ John B.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,13 @@ class Vote < ApplicationRecord
   belongs_to :vote_option
   belongs_to :user
 
-  
+  validates :unique_poll_vote?, inclusion: [true] 
+
+  def poll
+    vote_option.poll
+  end
+
+  def unique_poll_vote?
+    poll.votes.where(user_id: user.id).empty?
+  end
 end
